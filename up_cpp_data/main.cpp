14c85483727d9c453e2bf24a35b1d2c2ed25cf51
Sébastien Lavoie <seblav02@gmail.com>
2016-08-07 13:46:49 -0700

    Initial Commit

---
+++ up_cpp_data/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  up_cpp_data
+//
+//  Created by Sebastien Lavoie on 2016-08-07.
+//  Copyright © 2016 Sebastien Lavoie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
