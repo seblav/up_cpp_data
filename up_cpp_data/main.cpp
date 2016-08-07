47b65dedfe2a87e0c8bda24eb5c5a3b0ef914b6d
Sébastien Lavoie <seblav02@gmail.com>
2016-08-07 14:14:57 -0700

    Created program that shows a menu, ask for choice/quantity and return total cost

--- up_cpp_data/main.cpp
+++ up_cpp_data/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+float cost(float price, int quantity) {
+    return price * quantity;
+}
+
+int main() {
+    string menu[4] = {"Hamburger", "Fries", "Hot dog", "Salad"};
+    float price[4] = {4.75, 2.75, 3.75, 3.25};
+    int choice;
+    int quantity;
+    
+    cout<<"Here is the menu : "<<endl;
+    for (int i = 0; i < 4; i++) {
+        cout<<i+1<<". "<<menu[i]<<endl;
+    }
+    cout<<"Enter your selection : "<<endl;
+    cin>>choice;
+    cout<<"Enter the quantity : "<<endl;
+    cin>>quantity;
+    
+    cout<<"your total comes to : $"<<cost(price[choice-1], quantity)<<endl;
+    
     return 0;
 }
